2fa6c9ff3f6d78f33cdc15af37bd2e28ee0aa974
Veneiro <veneiroyt@gmail.com>
2024-10-03 13:48:12 +0200

    Github Actions now configured

--- main_linux.py
+++ main_linux.py
@@ -8,14 +8,15 @@ from dotenv import load_dotenv
 
 # Load variables
 load_dotenv("./TOKEN.env")
+discord.opus.load_opus("./lib/libopus.so.0.8.0")
 bot = commands.Bot(command_prefix='?', intents=discord.Intents.all(), activity = discord.Game(name="Ghetto Rally"))
 bot.remove_command("help")
 
 @bot.event
 async def on_ready():
     await bot.add_cog(help_cog(bot))
-    await bot.add_cog(music_cog(bot))
     await bot.add_cog(variety_cog(bot))
+    await bot.add_cog(music_cog(bot))
 
-token = os.getenv('DISCORD_TOKEN')
+token = os.getenv('TOKEN')
 bot.run(token)
